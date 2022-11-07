fcaa28900d8c60ee90367789049b966555dbd682
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-07 10:06:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [counters, setCounters] = useState([0,0]);
+  const [counters, setCounters] = useState([0]);
   
   return (
     <div className="App">
       <div>
-        <button onClick={()=> {
-          counters.push(0);
-        }}>Add counter</button>
+        {counters.length < 3 && <button onClick={()=> {
+         const newTab = [...counters];
+         newTab.push(0);
+         setCounters(newTab);
+        }}>Add counter</button>}
+       
         
 
         <div className='counters-div'>
         {counters.map((counter, index)=>{
-          return <div><button onClick={()=>{
+          return <div>
+            {counter > 0 ?<button onClick={()=>{
           const newTab = [...counters];
           newTab[index]= newTab[index] -1;
           setCounters(newTab);
-        }}>-</button>
+        }}>-</button> : <div className='empty-count'></div> }
+            
 
         <p>{counter}</p>
-        <button onClick={()=>{
+        {counter < 10 ?<button onClick={()=>{
           const newTab = [...counters];
           newTab[index] = newTab[index] +1;
           setCounters(newTab);
-        }}>+</button></div>
+        }}>+</button> : <div className='empty-count'></div> }
+        </div>
         })}</div>
      
       </div>
